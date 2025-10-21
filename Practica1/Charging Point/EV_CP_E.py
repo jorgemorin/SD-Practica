fe1f3c0c206d80ca82493e02364d901246980ddd
dam55-ua <dam55@alu.ua.es>
2025-10-21 16:30:54 +0200

    unicode

--- Practica1/Charging Point/EV_CP_E.py
+++ Practica1/Charging Point/EV_CP_E.py
@@ -7,21 +7,21 @@ import sys
 #CONEXION PARA AUTENTICACION CON CENTRAL
 def wait_for_monitor(cp_id):
     """
-    Engine espera una conexión directa del monitor (EV_CP_M).
-    El monitor debe validar su ID con Central antes de llegar aquí.
+    Engine espera una conexiï¿½n directa del monitor (EV_CP_M).
+    El monitor debe validar su ID con Central antes de llegar aquï¿½.
     """
     try:
         # Configura un nuevo socket para recibir al monitor
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s_monitor:
             s_monitor.bind(("127.0.0.2", "5001"))  # Usa un puerto distinto para el monitor
             s_monitor.listen(1)
-            print(f"[ENGINE] Esperando conexión del Monitor (CP_ID={cp_id}) en puerto {PORT + 1} ...")
+            print(f"[ENGINE] Esperando conexiï¿½n del Monitor (CP_ID={cp_id}) en puerto {PORT + 1} ...")
 
             conn, addr = s_monitor.accept()
             with conn:
                 print(f"[ENGINE] Monitor conectado desde {addr}")
 
-                # Recibir mensaje de autenticación del monitor
+                # Recibir mensaje de autenticaciï¿½n del monitor
                 mensaje = conn.recv(1024).decode('utf-8').strip()
                 print(f"[ENGINE] Mensaje del Monitor: {mensaje}")
 
@@ -29,7 +29,7 @@ def wait_for_monitor(cp_id):
                     print(f"[ENGINE] Monitor {cp_id} autenticado correctamente.")
                     conn.sendall(f"ACEPTADO:{cp_id}".encode('utf-8'))
                 else:
-                    print("[ENGINE] ID de monitor no válido.")
+                    print("[ENGINE] ID de monitor no vï¿½lido.")
                     conn.sendall(b"RECHAZADO:ID_invalido")
     except Exception as e:
         print(f"[ERROR MONITOR] {e}")
